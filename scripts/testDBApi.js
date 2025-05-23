0c0867217f76747ebc040db9828c79e4893d8a31
abdulsamed <abdalsamed71@gmail.com>
2025-05-23 17:42:11 +0000

    add config

--- scripts/testDBApi.js
+++ scripts/testDBApi.js
@@ -2,13 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = "https://nwkmcizenqgokebiuass.supabase.co"; // dev TODO
+const supabaseUrl = "https://flofeywjrxcklrizkgdg.supabase.co"; // dev TODO
 
 // fetch from supabase API and pass API Key as header
-const response = await fetch(`${supabaseUrl}/rest/v1/Gifts?select=gift_name,gift_code`, {
-  headers: {
-    apikey: process.env.SUPABASE_API_KEY,
-  },
-});
+const response = await fetch(
+  `${supabaseUrl}/rest/v1/Gifts?select=gift_name,gift_code`,
+  {
+    headers: {
+      apikey: process.env.SUPABASE_API_KEY,
+    },
+  }
+);
 const data = await response.json();
 console.log("DATA FROM DB:   ", data);
--- scripts/uploadGist.js
+++ scripts/uploadGist.js
@@ -3,22 +3,22 @@ const ethers = require("ethers");
 require("dotenv").config();
 
 const makeRequestSepolia = async () => {
-  // hardcoded for Avalanche Fuji
-  const routerAddress = "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0";
-  const donId = "fun-avalanche-fuji-1";
-  const rpcUrl = process.env.ETHEREUM_PROVIDER_AVALANCHEFUJI; // fetch Sepolia RPC URL
+  // hardcoded for SEPOLIA_RPC_URL
 
+  const routerAddress = "0xb83E47C2bC239B3bf370bc41e1459A34b41238D0";
+  const donId = "fun-ethereum-sepolia-1";
+  const rpcUrl = process.env.SEPOLIA_RPC_URL;
   const secrets = { apikey: process.env.SUPABASE_API_KEY };
 
   // Initialize ethers signer and provider to interact with the contracts onchain
   const privateKey = process.env.EVM_PRIVATE_KEY;
-  if (!privateKey) throw new Error("private key not provided - check your environment variables");
+  if (!privateKey)
+    throw new Error(
+      "private key not provided - check your environment variables"
+    );
 
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-
-  const wallet = new ethers.Wallet(privateKey);
-  const signer = wallet.connect(provider); // create ethers signer for signing transactions
-
+  const signer = new ethers.Wallet(privateKey, provider);
   //////// MAKE REQUEST ////////
 
   console.log("\nMake request...");
@@ -36,17 +36,25 @@ const makeRequestSepolia = async () => {
 
   console.log(`Creating gist...`);
   const githubApiToken = process.env.GITHUB_API_TOKEN;
-  if (!githubApiToken) throw new Error("githubApiToken not provided - check your environment variables");
+  if (!githubApiToken)
+    throw new Error(
+      "githubApiToken not provided - check your environment variables"
+    );
 
   // Create a new GitHub Gist to store the encrypted secrets
-  const gistURL = await createGist(githubApiToken, JSON.stringify(encryptedSecretsObj));
+  const gistURL = await createGist(
+    githubApiToken,
+    JSON.stringify(encryptedSecretsObj)
+  );
   console.log(`\n✅Gist created ${gistURL} . Encrypt the URLs..`);
-  const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([gistURL]);
+  const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([
+    gistURL,
+  ]);
 
   console.log(`\n✅Secrets encrypted. URLs: ${encryptedSecretsUrls}`);
 };
 
-makeRequestSepolia().catch(e => {
+makeRequestSepolia().catch((e) => {
   console.error(e);
   process.exit(1);
 });
