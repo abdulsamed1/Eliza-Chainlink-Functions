c4e7b80750f592400c4407032033c8ced47b4898
abdulsamed <abdalsamed71@gmail.com>
2025-05-26 23:29:39 +0300

    refactor: update import paths to use .js extension
    This change aligns with the project's migration to explicit file extensions in imports for better compatibility with ES modules

--- src/custom-plugins/actions/gift/utils/ParameterBuilder.ts
+++ src/custom-plugins/actions/gift/utils/ParameterBuilder.ts
@@ -9,8 +9,8 @@ import {
     type IAgentRuntime,
     type State,
 } from "@elizaos/core";
-import { WalletProvider } from "../../../providers/wallet";
-import { getGiftTemplate } from "../../../templates/index";
+import { WalletProvider } from "../../../providers/wallet.js";
+import { getGiftTemplate } from "../../../templates/index.js";
 import type { GetGiftParams } from "../../../types/index.js";
 
 export class ParameterBuilder {
