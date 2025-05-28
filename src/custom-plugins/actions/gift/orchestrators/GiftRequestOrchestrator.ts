a0da0a0cfdf2b10da9f24128c9d038d9483c3752
abdulsamed <abdalsamed71@gmail.com>
2025-05-28 21:40:06 +0000

    cofirm sepolia config now is done without any problems

--- src/custom-plugins/actions/gift/orchestrators/GiftRequestOrchestrator.ts
+++ src/custom-plugins/actions/gift/orchestrators/GiftRequestOrchestrator.ts
@@ -5,27 +5,34 @@
 import type { IAgentRuntime } from "@elizaos/core";
 import { initWalletProvider } from "../../../providers/wallet.ts";
 import type { GetGiftParams, Transaction } from "../../../types/index.ts";
-import { ConfigValidator, GiftContractConfigFactory } from "../config/GiftContractConfig.ts";
+import {
+  ConfigValidator,
+  GiftContractConfigFactory,
+} from "../config/GiftContractConfig.ts";
 import { GiftContractService } from "../services/GiftContractService.ts";
 
 export class GiftRequestOrchestrator {
-    constructor(private readonly contractService: GiftContractService) {}
+  constructor(private readonly contractService: GiftContractService) {}
 
-    async processGiftRequest(params: GetGiftParams): Promise<Transaction> {
-        console.log(`Processing gift request - Code: ${params.code}, Address: ${params.address}`);
-        
-        return await this.contractService.sendGiftRequest(params);
-    }
+  async processGiftRequest(params: GetGiftParams): Promise<Transaction> {
+    console.log(
+      `Processing gift request - Code: ${params.code}, Address: ${params.address}`
+    );
+
+    return await this.contractService.sendGiftRequest(params);
+  }
 }
 
 export class GiftRequestOrchestratorFactory {
-    static async create(runtime: IAgentRuntime): Promise<GiftRequestOrchestrator> {
-        const walletProvider = await initWalletProvider(runtime);
-        const config = GiftContractConfigFactory.createSepoliaConfig();
-        
-        ConfigValidator.validateConfig(config);
-        
-        const contractService = new GiftContractService(walletProvider, config);
-        return new GiftRequestOrchestrator(contractService);
-    }
+  static async create(
+    runtime: IAgentRuntime
+  ): Promise<GiftRequestOrchestrator> {
+    const walletProvider = await initWalletProvider(runtime);
+    const config = GiftContractConfigFactory.createSepoliaConfig();
+
+    ConfigValidator.validateConfig(config);
+
+    const contractService = new GiftContractService(walletProvider, config);
+    return new GiftRequestOrchestrator(contractService);
+  }
 }
